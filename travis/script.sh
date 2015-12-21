7d9c8b78e5f111809d716b2f5d6964aa484f41bf
Patrick Mick <patrickmick1@gmail.com>
2015-12-21 03:37:54 -0800

    Fixing invalid ios version for xctool

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-xctool -workspace NXTSegmentedControl.xcworkspace -scheme NXTSegmentedControl -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.1' test
+xctool -workspace NXTSegmentedControl.xcworkspace -scheme NXTSegmentedControl -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6,OS=8.1' test
