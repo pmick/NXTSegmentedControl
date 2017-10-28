2100512a0b27576d7b61efdb3ac7cc810faf1993
Arek Holko <fastred@users.noreply.github.com>
2017-10-28 20:13:37 +0200

    Carthage support (#12)
    * Add Carthage support
    
    * Update framework's version to match the newest git tag
    
    * Add building framework target on Travis

---
+++ NXTSegmentedControlFramework/NXTSegmentedControlFramework.h
@@ -0,0 +1,21 @@
+//
+//  NXTSegmentedControlFramework.h
+//  NXTSegmentedControlFramework
+//
+//  Created by Arkadiusz Holko on 10/10/2017.
+//  Copyright © 2017 Patrick Mick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NXTSegmentedControl.h"
+
+//! Project version number for NXTSegmentedControlFramework.
+FOUNDATION_EXPORT double NXTSegmentedControlFrameworkVersionNumber;
+
+//! Project version string for NXTSegmentedControlFramework.
+FOUNDATION_EXPORT const unsigned char NXTSegmentedControlFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NXTSegmentedControlFramework/PublicHeader.h>
+
+
